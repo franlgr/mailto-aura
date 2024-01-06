f8c4c58d7bc2ba9f4485199df55164f6320579c1
franlgr <grilfran90@gmail.com>
2024-01-06 20:33:30 -0300

    prod

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000; // Puedes cambiar el puerto si es necesario
+const port = process.env.PORT || 3000;
 
-// Configuración del middleware para servir archivos estáticos (como el HTML)
+// Configuración del middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta para manejar solicitudes a la raíz
@@ -12,7 +12,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Iniciar el servidor
-app.listen(port, () => {
-    console.log(`Servidor Node.js escuchando en http://localhost:${port}`);
-});
+// Iniciar el servidor solo si no estamos en un entorno de producción
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => {
+        console.log(`Servidor Node.js escuchando en http://localhost:${port}`);
+    });
+}
